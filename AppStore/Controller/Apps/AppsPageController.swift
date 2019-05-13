d19f8cbd7e0f6b2d8dda206038bd914b17e2ca47
Don McAllister <mcallisterda@gmail.com>
2019-05-13 14:40:56 -0400

    Adding Nested Header Collection View components

--- AppStore/Controller/Apps/AppsPageController.swift
+++ AppStore/Controller/Apps/AppsPageController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
+class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "id"
+    let headerId = "headerId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +19,24 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
         
         collectionView.register(
             AppsGroupCell.self, forCellWithReuseIdentifier: cellId)
+        
+        //1
+        collectionView.register(AppsPageHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
     }
+    
+    //2
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        return header
+
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: view.frame.width, height: 300)
+    }
+    
+    
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
---
+++ AppStore/Views/Apps/AppsPageHeader.swift
@@ -0,0 +1,26 @@
+//
+//  AppsPageHeader.swift
+//  AppStore
+//
+//  Created by Donald McAllister on 5/13/19.
+//  Copyright © 2019 Donald McAllister. All rights reserved.
+//
+
+import UIKit
+
+class AppsPageHeader: UICollectionReusableView {
+    
+    let appHeaderHorizontalController = AppsHeaderHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //appHeaderHorizontalController.view.backgroundColor = .purple
+        addSubview(appHeaderHorizontalController.view)
+        appHeaderHorizontalController.view.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
